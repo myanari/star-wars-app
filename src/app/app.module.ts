e28dcfcfd73657e47c964ca984b32ee4c1394129
Marcelle <yanarimy@gmail.com>
2018-05-02 19:33:20 -0300

    other components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
+import { ListComponent } from './list/list.component';
+import { ItemComponent } from './item/item.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabsComponent
+    TabsComponent,
+    ListComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
