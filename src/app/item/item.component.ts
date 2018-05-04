75a714408c0b0e4da0e55020529cb9d822cbfcfb
Marcelle <yanarimy@gmail.com>
2018-05-04 16:36:44 -0300

    polished making of user

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -7,22 +7,23 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
   @Input() char;
+  @Input() order;
 
   path = '';
   name = '';
-
-  makeUser() {
-    const names = this.name.split(' ');
-    if (names.length === 1 && names[0] !== 'bb8' && names[0] !== 'c3p0') {
-      names.push(Math.floor(Math.random() * 100).toString());
-    }
-    return names.join('');
-  }
+  user = '';
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.order);
     this.path = './assets/characters/' + this.char.name.toLowerCase() + '.svg';
     this.name = this.char.name.split('-').join(' ');
+
+    const names = this.name.split(' ');
+    if (names.length === 1 && names[0] !== 'bb8' && names[0] !== 'c3p0') {
+      names.push(Math.floor(Math.random() * 100).toString());
+    }
+    this.user = names.join('');
   }
 }
