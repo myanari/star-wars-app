cbf1a058795c4516309eb8e4d5db5a44aa5c4fec
Marcelle <yanarimy@gmail.com>
2018-05-04 05:07:38 -0300

    Simple and beautiful

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item',
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
+  @Input() name;
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() { }
 }
