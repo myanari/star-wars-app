4db5b53584b62bbdbafa840abc93991856f48710
Marcelle <yanarimy@gmail.com>
2018-05-04 06:35:39 -0300

    managing data

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.component.css']
 })
 export class TabsComponent implements OnInit {
+  characters = [
+    { name: 'Luke Skywalker', side: '' },
+    { name: 'Rey', side: '' },
+    { name: 'Darth Vader', side: '' },
+    { name: 'Kylo Ren', side: '' },
+  ];
+
+  chosenTab = 'all';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onChoose(side) {
+    this.chosenTab = side;
+  }
+
+  getCharacters() {
+    if (this.chosenTab === 'all') {
+      return this.chosenTab.slice();
+    }
+
+    return this.characters.filter((char) => {
+      return char.side === this.chosenTab;
+    });
+  }
 }
