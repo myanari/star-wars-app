6d83c2e446c0ea4ecbb1c98a03b6bcb29620191f
Marcelle <yanarimy@gmail.com>
2018-05-04 18:36:12 -0300

    moved characters list to service removed two unnecessary svgs

---
+++ src/app/star-wars.service.ts
@@ -0,0 +1,48 @@
+export class StarWarsService {
+  private characters = [
+    { name: 'admiral-ackbar', side: '' },
+    { name: 'bb8', side: '' },
+    { name: 'boba-fett', side: '' },
+    { name: 'c3p0', side: '' },
+    { name: 'captain-phasma', side: '' },
+    { name: 'chewbacca', side: '' },
+    { name: 'clone-trooper', side: '' },
+    { name: 'darth-maul', side: '' },
+    { name: 'darth-vader', side: '' },
+    { name: 'emperor-palpatine', side: '' },
+    { name: 'ewok', side: '' },
+    { name: 'finn', side: '' },
+    { name: 'general-hux', side: '' },
+    { name: 'general-leia', side: '' },
+    { name: 'greedo', side: '' },
+    { name: 'han-solo', side: '' },
+    { name: 'jabba-the-hutt', side: '' },
+    { name: 'jango-fett', side: '' },
+    { name: 'jawa', side: '' },
+    { name: 'kylo-ren', side: '' },
+    { name: 'lando-calrissian', side: '' },
+    { name: 'lobot', side: '' },
+    { name: 'luke-skywalker', side: '' },
+    { name: 'maz-kanata', side: '' },
+    { name: 'obiwan-kenobi', side: '' },
+    { name: 'poe-dameron', side: '' },
+    { name: 'princess-leia', side: '' },
+    { name: 'qui-gon-jinn', side: '' },
+    { name: 'r2d2', side: '' },
+    { name: 'red-five', side: '' },
+    { name: 'rey', side: '' },
+    { name: 'stormtrooper', side: '' },
+    { name: 'tusken-raider', side: '' },
+    { name: 'yoda', side: '' }
+  ];
+
+  getCharacters(chosenTab) {
+    if (chosenTab === 'all') {
+      return this.characters.slice();
+    }
+
+    return this.characters.filter((char) => {
+      return char.side === chosenTab;
+    });
+  }
+}
--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -6,43 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.component.css']
 })
 export class TabsComponent implements OnInit {
-  characters = [
-    { name: 'admiral-ackbar', side: '' },
-    { name: 'bb8', side: '' },
-    { name: 'boba-fett', side: '' },
-    { name: 'c3p0', side: '' },
-    { name: 'captain-phasma', side: '' },
-    { name: 'chewbacca', side: '' },
-    { name: 'clone-trooper', side: '' },
-    { name: 'darth-maul', side: '' },
-    { name: 'darth-vader', side: '' },
-    { name: 'emperor-palpatine', side: '' },
-    { name: 'ewok', side: '' },
-    { name: 'finn', side: '' },
-    { name: 'general-hux', side: '' },
-    { name: 'general-leia', side: '' },
-    { name: 'greedo', side: '' },
-    { name: 'han-solo', side: '' },
-    { name: 'jabba-the-hutt', side: '' },
-    { name: 'jango-fett', side: '' },
-    { name: 'jawa', side: '' },
-    { name: 'kylo-ren', side: '' },
-    { name: 'lando-calrissian', side: '' },
-    { name: 'lobot', side: '' },
-    { name: 'luke-skywalker', side: '' },
-    { name: 'maz-kanata', side: '' },
-    { name: 'obiwan-kenobi', side: '' },
-    { name: 'poe-dameron', side: '' },
-    { name: 'princess-leia', side: '' },
-    { name: 'qui-gon-jinn', side: '' },
-    { name: 'r2d2', side: '' },
-    { name: 'red-five', side: '' },
-    { name: 'rey', side: '' },
-    { name: 'stormtrooper', side: '' },
-    { name: 'tusken-raider', side: '' },
-    { name: 'yoda', side: '' }
-  ];
-
+  characters = [];
   chosenTab = 'all';
 
   constructor() { }
@@ -56,13 +20,7 @@ export class TabsComponent implements OnInit {
   }
 
   getCharacters() {
-    if (this.chosenTab === 'all') {
-      return this.characters.slice();
-    }
 
-    return this.characters.filter((char) => {
-      return char.side === this.chosenTab;
-    });
   }
 
   onSideChosen(charSide) {
