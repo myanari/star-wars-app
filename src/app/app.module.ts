c9cef6c8e4a3bd3fd9a84539df7ea7f653b3c468
Marcelle <yanarimy@gmail.com>
2018-05-09 01:45:12 -0300

    Create character component and manifest.json added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ListComponent } from './list/list.component';
 import { ItemComponent } from './item/item.component';
 import { StarWarsService } from './star-wars.service';
 import { LoggerService } from './logger.service';
+import { CreateCharacterComponent } from './create-character/create-character.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { LoggerService } from './logger.service';
     AppComponent,
     TabsComponent,
     ListComponent,
-    ItemComponent
+    ItemComponent,
+    CreateCharacterComponent
   ],
   imports: [
     BrowserModule
