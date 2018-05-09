826a5280be11151563ce5906410e1e4728e61f85
Marcelle <yanarimy@gmail.com>
2018-05-09 02:42:53 -0300

    Create character implemented and polished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styles: [
+    `button {
+      position: fixed;
+      bottom: 1rem;
+      right: 1rem;
+    }`
+  ]
 })
 export class AppComponent {
 }
