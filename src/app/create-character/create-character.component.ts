a1d422fcf6460a4bbd7ecd92d7883a8579af1801
Marcelle <yanarimy@gmail.com>
2018-05-11 21:18:08 -0300

    Cleaning up.

--- src/app/create-character/create-character.component.ts
+++ src/app/create-character/create-character.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { StarWarsService } from '../star-wars.service';
-import { LoggerService } from '../logger.service';
 
 @Component({
   selector: 'app-create-character',
-  templateUrl: './create-character.component.html',
-  styleUrls: ['./create-character.component.css']
+  templateUrl: './create-character.component.html'
 })
 export class CreateCharacterComponent implements OnInit {
   userWillAddOwnChar = false;
