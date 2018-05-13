a9375e2e99b42a7026ae9ae340d4565a3680beaa
Marcelle <yanarimy@gmail.com>
2018-05-13 01:01:05 -0300

    working finally, now it has an webpack error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { Ng2CompleterModule } from 'ng2-completer';
 
 import { StarWarsService } from './star-wars.service';
 import { LoggerService } from './logger.service';
@@ -12,8 +11,7 @@ import { TabsComponent } from './tabs/tabs.component';
 import { ListComponent } from './list/list.component';
 import { ItemComponent } from './item/item.component';
 import { HeaderComponent } from './header/header.component';
-import { UserDefinedCharacterComponent } from './create-character/user-defined-character/user-defined-character.component';
-import { PreDefinedCharacterComponent } from './create-character/pre-defined-character/pre-defined-character.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -21,15 +19,14 @@ import { PreDefinedCharacterComponent } from './create-character/pre-defined-cha
     TabsComponent,
     ListComponent,
     ItemComponent,
-    HeaderComponent,
-    UserDefinedCharacterComponent,
-    PreDefinedCharacterComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    Ng2CompleterModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [StarWarsService, LoggerService],
   bootstrap: [AppComponent]
--- src/app/create-character/create-character.module.ts
+++ src/app/create-character/create-character.module.ts
@@ -1,18 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Ng2CompleterModule } from 'ng2-completer';
+import { FormsModule, ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { CreateCharacterComponent } from './create-character.component';
+import { PreDefinedCharacterComponent} from './pre-defined-character/pre-defined-character.component';
+import { UserDefinedCharacterComponent} from './user-defined-character/user-defined-character.component';
+import { ListComponent} from '../list/list.component';
+import { HeaderComponent} from '../header/header.component';
+import { TabsComponent} from '../tabs/tabs.component';
+import { ItemComponent} from '../item/item.component';
+import { HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    CreateCharacterComponent
+    TabsComponent,
+    ListComponent,
+    ItemComponent,
+    HeaderComponent,
+    UserDefinedCharacterComponent,
+    PreDefinedCharacterComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([
       { path: '', component: CreateCharacterComponent }
-    ])
+    ]),
+    HttpClientModule,
+    Ng2CompleterModule,
+    ReactiveFormsModule,
+    FormGroup
+  ],
+  exports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FormGroup,
+    Ng2CompleterModule
   ]
 })
 export class CreateCharacterModule {}
