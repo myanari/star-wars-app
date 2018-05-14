4652a872d78bd9e58e94b8c160f74d236b632bd3
Marcelle <yanarimy@gmail.com>
2018-05-14 02:29:20 -0300

    added responsiveness to footer but it's still fixed urggg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ListComponent } from './list/list.component';
 import { ItemComponent } from './item/item.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ExternalLinkHandlerDirective } from './external-link-handler.directive';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { FooterComponent } from './footer/footer.component';
     ListComponent,
     ItemComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ExternalLinkHandlerDirective
   ],
   imports: [
     BrowserModule,
