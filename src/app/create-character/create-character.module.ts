a78dc95b5fc0192b41b35dded97a04dfe40234da
Marcelle <yanarimy@gmail.com>
2018-05-16 03:57:31 -0300

    i found another toggler thank god also added another component to handle hamburger icon

--- src/app/create-character/create-character.module.ts
+++ src/app/create-character/create-character.module.ts
@@ -8,10 +8,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CreateCharacterComponent } from './create-character.component';
 import { UserDefinedCharacterComponent } from './user-defined-character/user-defined-character.component';
 import { PreDefinedCharacterComponent } from './pre-defined-character/pre-defined-character.component';
-import { ItemComponent } from '../item/item.component';
-
-import { StarWarsService } from '../star-wars.service';
-import { LoggerService } from '../logger.service';
 
 @NgModule({
   declarations: [
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   isActive = false;
+  @HostListener('click') click() {
+    this.isActive = !this.isActive;
+  }
 
   constructor() { }
 
